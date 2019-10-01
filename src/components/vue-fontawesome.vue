d2c1880d44a988c092fd5d4571da5d3d025dfec8
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-10-01 18:55:21 +0530

    created component

---
+++ src/components/vue-fontawesome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <i class="fa fa-telegram" aria-hidden="true"></i>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vue-fontawesome",
+  props: ["icon", "color", "size"]
+  //   data() {
+  //     return {};
+  //   }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>I am Home</h2>
+    <VueFontawesome></VueFontawesome>
+    <vue-fontawesome></vue-fontawesome>
+  </div>
+</template>
+
+<script>
+import VueFontawesome from "@/components/vue-fontawesome.vue";
+export default {
+  name: "Home",
+  components: {
+    VueFontawesome
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
